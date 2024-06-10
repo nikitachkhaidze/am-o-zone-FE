d505229ca90898aeedbf0ce037fb6ee3b7d7d5e8
Nikita Chkhaidze <nikita.chkhaidze@gmail.com>
2024-06-10 21:55:41 +0400

    reverted to standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { LayoutComponent } from '../layout/layout.component';
 
 @Component({
   selector: 'am-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [LayoutComponent],
 })
 export class AppComponent {
   title = 'am-o-zone-fe';
--- src/layout/layout.component.ts
+++ src/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostBinding } from '@angular/core';
+import { ThemeButtonComponent } from '../app/shared/theme-button/theme-button.component';
 
 enum Theme {
   Light = 'light',
@@ -9,6 +10,10 @@ enum Theme {
   selector: 'am-layout',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
+  standalone: true,
+  imports: [
+    ThemeButtonComponent,
+  ],
 })
 export class LayoutComponent {
   @HostBinding('attr.data-theme') theme = Theme.Light;
