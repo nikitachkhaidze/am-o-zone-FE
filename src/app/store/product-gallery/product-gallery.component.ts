8d1e6dfd4e4e4eed7e1722bc8c66567a2329dc73
Nikita Chkhaidze <nikita.chkhaidze@gmail.com>
2024-08-15 17:48:22 +0400

    Revert "moved search bar to product gallery"
    This reverts commit 1fc42b760390fbd6e312fa0167f1a46f1d9c1b78.

--- src/app/store/product-gallery/product-gallery.component.ts
+++ src/app/store/product-gallery/product-gallery.component.ts
@@ -3,11 +3,9 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Product } from '../../types/ui/product.interface';
 import { ProductListItemComponent } from './product-list-item/product-list-item.component';
 import { ProductsState } from '../../state/store/products/products.state';
-import { SearchBarComponent } from '../../shared/search-bar/search-bar.component';
 
 @Component({
   selector: 'am-product-gallery',
@@ -16,8 +14,6 @@ import { SearchBarComponent } from '../../shared/search-bar/search-bar.component
     MatPaginatorModule,
     ProductListItemComponent,
     AsyncPipe,
-    SearchBarComponent,
-    ReactiveFormsModule,
   ],
   templateUrl: './product-gallery.component.html',
   styleUrl: './product-gallery.component.scss',
@@ -25,7 +21,6 @@ import { SearchBarComponent } from '../../shared/search-bar/search-bar.component
 })
 export class ProductGalleryComponent {
   products: Observable<Product[]> = this.store.select(ProductsState.products);
-  searchControl = new FormControl('');
 
   constructor(private store: Store) {
   }
